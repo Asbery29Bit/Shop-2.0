2e166e89643e537454c98a2bf720f2296a40abc3
fyfyfc2912@gmail.com <fyfyfc2912@gmail.com>
2025-01-19 06:16:46 +0000

    Edited files:     /src/function.js

---
+++ src/function.js
@@ -0,0 +1,45 @@
+js
+function deleteFromCart(name) \{
+    var $session = $jsapi.context().session;
+    for (var i = 0; i < $session.cart.length; i++) \{
+        var current_position = $session.cart[i];
+        if (current_position.name === name) \{
+            $session.cart.splice(i, 1);
+        \}
+    \}
+    log('!!!!!! CART: ' + JSON.stringify($session.cart));
+\}
+
+function getTotalSum() \{
+    var totalSum = 0;
+    var $session = $jsapi.context().session;
+
+    for (var i = 0; i < $session.cart.length; i++) \{
+        var current_position = $session.cart[i];
+        for (var id = 1; id < Object.keys(plants).length + 1; id++) \{
+            if (current_position.name === plants[id].value.name) \{
+                totalSum += current_position.price * current_position.quantity;
+            \}
+        \}
+    \}
+    log("!!!!!!!!!!!! totalSum = " + totalSum);
+    return totalSum;
+\}
+
+function editText(messageId, text) \{
+    var $response = $jsapi.context().response;
+    var reply = \{
+        type: "raw",
+        body: \{
+            text: text,
+            message_id: messageId,
+            reply_markup: \{
+                "resize_keyboard": false,
+            \}
+        \},
+        method: "editMessageText"
+    \};
+
+    $response.replies = $response.replies || [];
+    $response.replies.push(reply);
+\}
